8bf3e49c41edf7eb2cb9e37cf5ee6a83eb7e8fb8
Daniel Siedentopf <d.siedentopf@dieboldnixdorf.com>
2020-01-08 15:04:54 +0100

    new escrow times

--- escrowList.ts
+++ escrowList.ts
@@ -1,6 +1,6 @@
 export const escrows:any = [
         {
-            executeAfter: "2020-01-08 15:00:00",
+            executeAfter: "2020-01-08 14:12:00",
             escrowList: [
                 {
                     account: "rpzR63sAd7fc4tR9c8k6MR3xhcZSpTAYKm",
@@ -13,24 +13,7 @@ export const escrows:any = [
             ]
         },
         {
-            executeAfter: "2020-01-08 14:45:00",
-            escrowList: [
-                {
-                    account: "rpzR63sAd7fc4tR9c8k6MR3xhcZSpTAYKm",
-                    sequence: 81
-                },
-                {
-                    account: "rpzR63sAd7fc4tR9c8k6MR3xhcZSpTAYKm",
-                    sequence: 82
-                },
-                {
-                    account: "rpzR63sAd7fc4tR9c8k6MR3xhcZSpTAYKm",
-                    sequence: 83
-                },
-            ]
-        },
-        {
-            executeAfter: "2020-01-08 14:50:00",
+            executeAfter: "2020-01-08 14:15:00",
             escrowList: [
                 {
                     account: "rpzR63sAd7fc4tR9c8k6MR3xhcZSpTAYKm",
